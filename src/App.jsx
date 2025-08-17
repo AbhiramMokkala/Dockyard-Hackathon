0123e2643814cd0baad0e2597eef4a795ee0f143
Abhiram <abhirammokkala@gmail.com>
2025-08-17 15:32:05 +0530

    Configure GitHub Pages deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage";
 import AboutPage from "./pages/aboutpage";
 import ProgramsPage from "./pages/programspage";
